3f495660561ed09088a16910b58984afc810d91a
root <efemiayafavour@gmail.com>
2022-12-01 01:29:38 -0800

    done

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    if i == 113 or i == 101:
-        continue
-    print("{:c}".format(i), end="")
+for i in 'abcdefghijklmnopqrstuvwxyz':
+    if i != 'e' and i != 'q':
+        print("{}".format(i), end="")
