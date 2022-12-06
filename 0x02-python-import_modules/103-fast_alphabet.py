18baf4c62e820435dac6eef9cec2309c9aab92e2
root <efemiayafavour@gmail.com>
2022-12-06 11:48:20 -0800

    done

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,2 +1,2 @@
-#!/usr/bin/python3
+i!/usr/bin/python3
 import alpha
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from hidden_4 import my_secret_santa, print_hidden, print_school
 if __name__ == "__main__":
-    print("my_secret_santa")
-    print("print_hidden")
-    print("print_school")
+    from hidden_4 import *
+    arr = dir()
+    for i in range(0, len(arr)):
+        if arr[i][0:2] != "__":
+            print("{}".format(arr[i]))
