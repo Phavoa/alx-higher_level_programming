e1891bd3a591a48ab6a4e60ac92f047f67edbb5c
root <efemiayafavour@gmail.com>
2023-04-16 11:07:35 -0700

    done

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,6 +5,5 @@
 
 
 def is_kind_of_class(obj, a_class):
-     """Returns True if obj is an instance of, or if obj
-     is an instance of a class that inherited from, a_class; otherwise False."""
+     """True if obj is an instance or inherited from a_class, else False"""
     return isinstance(obj, a_class)
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+7-base_geometry.txt#!/usr/bin/python3
 """
 7-base_geometry module
 """
