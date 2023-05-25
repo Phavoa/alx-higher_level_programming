6dfcec8c560d1d0a0a773b84d6a4b3f85b82df2b
Phavoa <efemiayafavour@gmail.com>
2023-05-25 06:57:33 -0700

    coreected again

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 0-read_file module
 """
@@ -6,10 +7,11 @@
 
 def read_file(filename=""):
     """
-    read_file - function to read and print print in stdout
+    Function to read a text file (UTF8) and print its contents to stdout.
+    
     Args:
-        filename: name of the file
+        filename (str): Name of the file to be read.
     """
-    with open(filename, mode= "r", encoding="UTF-8") as f:
+    with open(filename, mode="r", encoding="UTF-8") as f:
         for line in f:
             print(line, end="")
