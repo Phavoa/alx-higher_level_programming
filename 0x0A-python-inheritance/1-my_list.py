e186a39e9cae55a8cfbb519895278facd0ecf50a
root <efemiayafavour@gmail.com>
2023-06-11 21:30:14 -0700

    task one completion

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,5 @@
 #!/usr/bin/pytihon3
 class MyList(list):
-    """ initialising the object"""
-    def __init__(self):
-        super().__init__()
-    
     """ define public instance method """
     def print_sorted(self):
         """ print sorted list """
