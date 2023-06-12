569384c79b18b71578326dc3b6dde1d0dbc5b323
root <efemiayafavour@gmail.com>
2023-06-11 22:04:43 -0700

    created a doctest for 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/pytihon3
 class MyList(list):
+    """ initialising the object"""
+    def __init__(self):
+        super().__init__()
+    
     """ define public instance method """
     def print_sorted(self):
         """ print sorted list """
