6132528407155fc99fb2e903591e572e809fa2b8
root <efemiayafavour@gmail.com>
2023-06-13 12:57:34 -0700

    completed the first tast for reading a txt file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -13,4 +13,4 @@ def read_file(filename=""):
         filename : Name of the file to be read.
     """
     with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read())
+        print(f.read(), end="")
