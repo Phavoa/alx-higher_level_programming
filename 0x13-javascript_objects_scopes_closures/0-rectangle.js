54e2d46bf8514e206ee33351ff515ece31b54bb0
Efemiaya Favour Oghenetega <EfemiayaFavour@gmail.com>
2023-11-15 02:31:27 +0100

    completed task 0 through task 6

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+class Rectangle {
+}
+module.exports = Rectangle;
+
+
+
+
+const r1 = new Rectangle();
+console.log(r1);
+console.log(r1.constructor);
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
+};
+
+
+const s1 = new Square(4);
+s1.charPrint();
+
+s1.charPrint('C');
